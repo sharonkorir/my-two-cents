11e7cd7f309195e181ceee0c804df1bcf2cb9853
sharonkorir <sharon.korir@student.moringaschool.com>
2022-03-14 09:00:42 +0300

    deployment to heroku

--- config.py
+++ config.py
@@ -19,8 +19,7 @@ class ProdConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://< your heroku url here>'
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     
   
     
